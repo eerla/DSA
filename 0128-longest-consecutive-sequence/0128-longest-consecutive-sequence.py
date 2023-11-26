1d13ad6b20fd26a0a4ecf7e48e2ffa374205c4fa
eerla <gurubrahmam443@gmail.com>
2023-11-25 21:34:54 -0500

    Time: 369 ms (93.54%), Space: 31.5 MB (38.80%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,34 +1,34 @@
-class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-        uniqsortedlist = sorted(set(nums))
-        maxL, curL = 1, 1
-        
-        for i in range(len(uniqsortedlist)):
-            if uniqsortedlist[i] - uniqsortedlist[i-1] == 1 and i > 0:
-                curL += 1
-                maxL = max(curL, maxL)
-            else:
-                curL = 1
-                i-=1
-        return maxL
-    
-
 # class Solution:
 #     def longestConsecutive(self, nums: List[int]) -> int:
-#         numSet = set(nums)
-#         longest = 0
+#         if not nums:
+#             return 0
+        
+#         uniqsortedlist = sorted(set(nums))
+#         maxL, curL = 1, 1
+        
+#         for i in range(len(uniqsortedlist)):
+#             if uniqsortedlist[i] - uniqsortedlist[i-1] == 1 and i > 0:
+#                 curL += 1
+#                 maxL = max(curL, maxL)
+#             else:
+#                 curL = 1
+#                 i-=1
+#         return maxL
+    
 
-#         for n in numSet:
-#             # check if its the start of a sequence
-#             if (n - 1) not in numSet:
-#                 length = 1
-#                 while (n + length) in numSet:
-#                     length += 1
-#                 longest = max(length, longest)
-#         return longest
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for n in numSet:
+            # check if its the start of a sequence
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
 
                 
                 

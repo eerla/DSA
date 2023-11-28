040a92cedd65343bf0142cab574173660b3a8c50
eerla <gurubrahmam443@gmail.com>
2023-11-27 21:43:53 -0500

    Time: 41 ms (59.82%), Space: 16.6 MB (36.12%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        def backtrack(openN, closedN, path):
+            if openN == closedN == n:
+                res.append(path)
+            
+            if openN < n:
+                backtrack(openN+1, closedN, path+'(')
+            if closedN < openN:
+                backtrack(openN, closedN+1, path+')')
+
+            return res
+        
+        ans = backtrack(0,0,'')
+        return ans

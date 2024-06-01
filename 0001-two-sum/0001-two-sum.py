1cfc273321731d909620d89534da7f04add02d3d
eerla <gurubrahmam443@gmail.com>
2024-06-01 11:47:41 -0400

    Time: 42 ms (99.59%), Space: 17.8 MB (15.60%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        balance = {}
         
-        for i in range(len(nums)):
-            bal = target - nums[i]
-                
-            if bal in balance:
-                return [i, balance[bal]]
-            balance[nums[i]] = i
+        visited = {}
+        for i, n in enumerate(nums):
+            rem = target-n
             
+            if rem in visited:
+                return [i, visited[rem]]
+            visited[n] = i
+            
+            
+#         for i in range(len(nums)):
+#             bal = target - nums[i]
                 
+#             if bal in balance:
+#                 return [i, balance[bal]]
+#             balance[nums[i]] = i
